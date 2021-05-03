245171801bb6b1b16f075566de2daae0e5059075
Micky Abir <micky.abir@gmail.com>
2021-05-03 15:25:10 -0700

    adds all common and market API

--- coinex/coinex.py
+++ coinex/coinex.py
@@ -64,3 +64,66 @@ class CoinEx:
         """uses `expandPathToUrl()` to make the API call"""
         url = CoinEx.expandPathToUrl(path, params)
         return requests.get(url)
+
+## COMMON FUNCTIONS ---------------
+
+    def getCurrencyRates():
+        res = CoinEx.request('common/currency/rate')
+        rjson = res.json()
+        return CoinExResponse(res.ok and rjson['code'] == 0, rjson['message'], rjson['data'])
+
+    def getAssetConfig(coin_type = ''):
+        params = {'coin_type': coin_type}
+        res = CoinEx.request('common/asset/config', params)
+        rjson = res.json()
+        return CoinExResponse(res.ok and rjson['code'] == 0, rjson['message'], rjson['data'])
+
+## MARKET FUNCTIONS ---------------
+
+    def getMarketList():
+        res = CoinEx.request('market/list')
+        rjson = res.json()
+        return CoinExResponse(res.ok and rjson['code'] == 0, rjson['message'], rjson['data'])
+
+    def getMarketStatistics(market):
+        params = {'market': market}
+        res = CoinEx.request('market/ticker', params)
+        rjson = res.json()
+        return CoinExResponse(res.ok and rjson['code'] == 0, rjson['message'], rjson['data'])
+
+    def getMarketDepth(market, merge, limit = 20):
+        params = {'market': market, 'merge': merge, 'limit': str(limit)}
+        res = CoinEx.request('market/depth', params)
+        rjson = res.json()
+        return CoinExResponse(res.ok and rjson['code'] == 0, rjson['message'], rjson['data'])
+
+    def getMarketTransactions(market, last_id = 0, limit = 100):
+        params = {'market': market, 'last_id': str(last_id), 'limit': str(limit)}
+        res = CoinEx.request('market/deals', params)
+        rjson = res.json()
+        return CoinExResponse(res.ok and rjson['code'] == 0, rjson['message'], rjson['data'])
+
+    def getMarketKLineData(market, ttype, limit = 100):
+        params = {'market': market, 'type': ttype, 'limit': str(limit)}
+        res = CoinEx.request('market/deals', params)
+        rjson = res.json()
+        return CoinExResponse(res.ok and rjson['code'] == 0, rjson['message'], rjson['data'])
+
+    def getMarketInfo():
+        res = CoinEx.request('market/info')
+        rjson = res.json()
+        return CoinExResponse(res.ok and rjson['code'] == 0, rjson['message'], rjson['data'])
+
+    def getMarketInfoSingle(market):
+        params = {'market': market}
+        res = CoinEx.request('market/detail', params)
+        rjson = res.json()
+        return CoinExResponse(res.ok and rjson['code'] == 0, rjson['message'], rjson['data'])
+
+    def getMarketAMM():
+        res = CoinEx.request('amm/market')
+        rjson = res.json()
+        return CoinExResponse(res.ok and rjson['code'] == 0, rjson['message'], rjson['data'])
+
+if __name__ == '__main__':
+    print(CoinEx.getMarketAMM())
